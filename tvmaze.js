3f9af6e7ee38acf117d025063fdc7d43d336e87e
Bruno Martins <bscmartins@gmail.com>
2015-11-15 23:00:02 +0000

    Added remaining endpoints

--- tvmaze.js
+++ tvmaze.js
@@ -40,6 +40,10 @@
 		return request(this, 'singlesearch/shows?q=' +  search, callback);
 	};	
 
+	TVMaze.prototype.show_lookup = function(id,id_type,callback) {
+		return request(this, 'lookup/shows?' + id_type +'=' + id, callback);
+	};
+
 	TVMaze.prototype.show_episode_list = function(show_id, show_specials,callback) {
 		var specials = 0;
 		if (show_specials) {
@@ -48,21 +52,64 @@
 		return request(this, 'shows/' + show_id + '/episodes?specials=' + specials, callback);
 	};	
 
-/*
-	show_lookup
-	show_cast
-	show_akas
-	show_index
-	episode_by_number
-	episodes_by_date
-	schedule
-	full_schedule
-	people_search
-	people
-	person_cast_credits
-	person_crew_credits
-	updates
-*/
+	TVMaze.prototype.show_cast = function(id,callback) {
+		return request(this, 'shows/' + id + '/cast', callback);
+	};	
+
+	TVMaze.prototype.show_akas = function(id,callback) {
+		return request(this, 'shows/' + id + '/akas', callback);
+	};	
+
+	TVMaze.prototype.show_index = function(page, callback) {
+		return request(this, 'shows?page=' + page , callback);
+	};	
+
+	TVMaze.prototype.episode_by_number = function(id,season,number, callback) {
+		return request(this, 'shows/' + id + '/episodebynumber?season=' + season + '&number=' + number, callback);
+	};	
+
+	TVMaze.prototype.episode_by_date = function(id,date, callback) {
+		return request(this, 'shows/' + id + '/episodesbydate?date=' + date  , callback);
+	};	
+
+	TVMaze.prototype.schedule = function(country_code,date,  callback) {
+		return request(this, 'schedule?country=' + country_code + '&date=' + date  , callback);
+	};
+
+	TVMaze.prototype.full_schedule = function(callback) {
+		return request(this, 'schedule/full' , callback);
+	};
+
+	TVMaze.prototype.people_search = function(search, callback) {
+		return request(this, 'search/people?q=' + search , callback);
+	};
+
+	TVMaze.prototype.people = function(id, callback) {
+		return request(this, 'people/' + id , callback);
+	};
+
+	TVMaze.prototype.person_cast_credits = function(id, embed, callback) {
+		var uri = 'people/' + id + '/castcredits';
+		var embed_shows = 0;
+		if (embed) {
+			uri = uri + '?embed=show';
+		}
+		return request(this, uri, callback);
+	};
+
+	TVMaze.prototype.person_crew_credits = function(id, embed, callback) {
+		var uri = 'people/' + id + '/crewcredits';
+		var embed_shows = 0;
+		if (embed) {
+			uri = uri + '?embed=show';
+		}
+		return request(this, uri, callback);
+	};
+
+	TVMaze.prototype.updates = function(callback) {
+		return request(this, 'updates/shows' , callback);
+	};
+
 
 
       window.TVMaze = TVMaze;
